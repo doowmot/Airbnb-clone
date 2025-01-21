29a79b22f489b8e40231d7cfc6d2067ad94b6338
Tom Wood <tomwood1875@gmail.com>
2025-01-21 12:39:29 +0000

    Update space.py and test_space.py

--- lib/space.py
+++ lib/space.py
@@ -1,19 +1,25 @@
 class Space:
     # We initialise with all of our attributes
     # Each column in the table should have an attribute here
-    def __init__(self, id, listing_id, name, description, price):
+    def __init__(self, id, listing_id, name, description, price_per_night):
+
+        if name == "":
+            raise ValueError("Name cannot be empty")
+        if not isinstance(name, str):
+            raise ValueError("Name must be text")
+        if price_per_night <= 0:
+            raise ValueError("Price must be above 0")
+
         self.id = id
         self.listing_id = listing_id
         self.name = name
         self.description = description
-        self.price = price
-
+        self.price_per_night = price_per_night
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
     
-    
     def __repr__(self):
-        return f"Space({self.id}, {self.listing_id}, '{self.name}', {self.description}, {self.price})"
+        return f"Space({self.id}, {self.listing_id}, '{self.name}', '{self.description}', {self.price_per_night})"
 
 
