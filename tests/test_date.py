6222d028697a2dec2efbfb55cf30ed45bd0fc8d4
Tom Wood <tomwood1875@gmail.com>
2025-01-22 12:28:34 +0000

    Fixed test failing in test_date.py

--- tests/test_date.py
+++ tests/test_date.py
@@ -7,7 +7,7 @@ def test_date_constructs():
     date = Date(1,"2025-01-01",True, 1)
     assert date.date_id == 1
     assert date.date == "2025-01-01"
-    assert date.avaliable == True
+    assert date.available == True
     assert date.space_id == 1
 
 """
